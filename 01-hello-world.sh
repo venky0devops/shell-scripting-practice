875fecb77bca01038445a4d0b4ee0f70303c10b1
developerone-contributor <developerone-contributor@gmail.com>
2026-01-29 12:07:18 +0530

    hello world completed

---
+++ 01-hello-world.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+##!/bin/bash we called it as shebang(#! is called a shebang and /bin/bash is the interpreter.).
+#The shebang tells the OS which interpreter to use to run the script
+#this is comment, this will not be executed by bash, this is only for information purpose. 
+echo "Welcome to Hello World"
