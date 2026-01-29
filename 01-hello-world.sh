fa735eb9ea27d6a334c89e2742d969f666bad5e7
venkydevopslearning <venkydevopslearning@gmail.com>
2026-01-29 12:30:44 +0530

    update the data

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -3,3 +3,4 @@
 #The shebang tells the OS which interpreter to use to run the script
 #this is comment, this will not be executed by bash, this is only for information purpose. 
 echo "Welcome to Hello World"
+echo "this is second line of Hello world to update the details in this file"
